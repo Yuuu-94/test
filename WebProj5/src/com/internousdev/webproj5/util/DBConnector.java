715869e81d6ff12d9516bf7ecffc46eea1d595f8
Yuuu-94 <yuuujam84@gmail.com>
2019-07-01 21:33:12 +0900

    hello

--- WebProj5/src/com/internousdev/webproj5/util/DBConnector.java
+++ WebProj5/src/com/internousdev/webproj5/util/DBConnector.java
@@ -1,4 +1,4 @@
-package com.internousdev.webproj4.util;
+package com.internousdev.webproj5.util;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
@@ -25,7 +25,6 @@ public class DBConnector {
 			e.printStackTrace();
 		}
 		return con;
-
 	}
 
 }
